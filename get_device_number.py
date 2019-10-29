7f0d82c08b91fd5fad1bb4c7444194203389b86d
Yoshida Rick <chikara.f.yoshida@gmail.com>
2019-10-29 11:11:56 +0900

    二段階認証を得るためのget_twoWay_Auth.pyを追加

--- get_device_number.py
+++ get_device_number.py
@@ -1,15 +1,19 @@
 from pyicloud import PyiCloudService
 
-#接続するicloudのメールアドレスとパスワードを保存
+# 接続するicloudのメールアドレスとパスワードを保存
 api = PyiCloudService('ychikara@unomaha.edu', 'Bakabaka0208')
 
-#iphoneのGPS情報を取得
+
+# iphoneのGPS情報を取得
 def get_oauth():
     auth = api.devices
     return auth
 
-#icloudに登録している端末のデバイスナンバーを取得
+
+# icloudに登録している端末のデバイスナンバーを取得
 if __name__ == '__main__':
-    auth=list(get_oauth())
+    auth = list(get_oauth())
     print('\n')
-    [print(i) for i in range(len(auth))if str(auth[i]) == 'iPhone 11: iPhone']
+    [print(i) for i in range(len(auth)) if str(auth[i]) == 'iPhone 11: iPhone']
+    auth1 = auth.copy()
+    print(auth1)
