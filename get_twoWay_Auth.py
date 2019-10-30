6985583497c326ed2215a26af9d0e3bf5f1916c4
Yoshida Rick <chikara.f.yoshida@gmail.com>
2019-10-30 15:39:18 +0900

    ＜エラー＞ sqlite_test.py 8行目 sqlite3.OperationalError: near "not": syntax error すでにproduct テーブルが存在する状態で create table if not exists product() としたいのだが、SQLようわからん。

--- get_twoWay_Auth.py
+++ get_twoWay_Auth.py
@@ -1,9 +1,7 @@
 from pyicloud import PyiCloudService
-api = PyiCloudService(api = PyiCloudService('ychikara@unomaha.edu', 'Bakabaka0208'))
 
+api = PyiCloudService(api=PyiCloudService('ychikara@unomaha.edu', 'Bakabaka0208'))
 
-#二段階認証を得る
+# 二段階認証を得る
 if api.requires_2fa:
-    import clidk
-
-
+    import click
